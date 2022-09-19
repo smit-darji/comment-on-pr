123e6d167b8edb3fea9ceb08fe96f695666eab9c
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 15:43:39 +0530

    first commit

--- .github/workflows/template.md
+++ .github/workflows/template.md
@@ -3,4 +3,4 @@ hiii
 git fetch upstream pull/{pull_id}/head:{branch_name}
 git checkout {branch_name}
 ```
-invalid fine name is : {fine_name}
+invalid fine name is : {invalid_file_names}
--- entrypoint.py
+++ entrypoint.py
@@ -82,7 +82,7 @@ def main():
 
     # build a comment
     pr_info = {
-        'fine_name :': m.invalid_file_names,
+        'invalid_file_names :': m.invalid_file_names,
         'pull_id': pr.number,
         'branch_name': branch_name
     }
