8d992788633a432bbe6494381243c213e23b945b
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 20:03:13 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -1,4 +1,3 @@
-from collections import UserDict
 from github import Github
 from main import GITHUB_TOKEN
 
@@ -7,7 +6,7 @@ def post_pr_comment (github_client, invalid_file_names, invalid_directory_names)
   print(invalid_directory_names)
   # g = Github("access_token")
   gh = Github(GITHUB_TOKEN)
-  repo = gh.repository(UserDict, repo_name)
+  repo = gh.repository(user, repo_name)
   pr = repo.create_pull(description, base, from_branch, detailed)
   issue = repo.issue(pr.number)
   issue.create_comment("test :",invalid_file_names)
