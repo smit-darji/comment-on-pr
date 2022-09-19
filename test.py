09035f18efbf6f69d70ba484944df89e2daf1e05
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-19 15:30:13 +0530

    first commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
-env_file = os.getenv('GITHUB_ENV')
+# env_file = os.getenv('GITHUB_ENV')
 
-with open(env_file, "a") as myfile:
-    myfile.write("MY_VAR=MY_VALUE")
+# with open(env_file, "a") as myfile:
+#     myfile.write("MY_VAR=MY_VALUE")
