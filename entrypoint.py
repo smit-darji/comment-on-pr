91721fd5090ef9658757e5079d11d5ab277ea200
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-20 11:05:00 +0530

    first commit

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@ import imp
 import json
 import os
 from github import Github
-
+from main import *
 # from .validationmain import invalid_file_names
 
 def read_json(filepath):
@@ -75,7 +75,7 @@ def main():
     repo = gh.get_repo(event['repository']['full_name'])
     prs = repo.get_pulls(state='open', sort='created', head=branch_label)
     pr = prs[0]
-    filenamevalidation = "invalid_file_names"
+    filenamevalidation = main.invalid_file_names
     # load template
     template = load_template(get_actions_input('filename'))
 
