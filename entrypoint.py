43367d0f9e12de6b36ed28037574becd564cfa65
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-20 11:42:32 +0530

    first commit

--- entrypoint.py
+++ entrypoint.py
@@ -75,7 +75,7 @@ def main():
     repo = gh.get_repo(event['repository']['full_name'])
     prs = repo.get_pulls(state='open', sort='created', head=branch_label)
     pr = prs[0]
-    filenamevalidation = main.invalid_file_names
+    filenamevalidation = "smit"
     # load template
     template = load_template(get_actions_input('filename'))
 
