61377071f2f00ec69c2ba725cb135f10cff846b8
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-20 14:55:04 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -103,7 +103,7 @@ def post_pr_comment (github_client, invalid_file_names, invalid_directory_names)
         exit(0)
 
     # add the comment
-    pr.create_issue_comment(new_comment)
+    pr.create_issue_comment(pr_info)
 
 
 
