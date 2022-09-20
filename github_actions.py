bca6076c263ec637fa1b1e9f77c9de2293945dc5
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-20 12:32:45 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -15,34 +15,7 @@ def post_pr_comment (github_client, invalid_file_names, invalid_directory_names)
     print(type(invalid_file_names))
     invalid_file_names_Stirng = " ".join( invalid_file_names )
     print(invalid_file_names_Stirng)
-    print (type(invalid_file_names_Stirng))  
-    gh = Github(os.getenv('GITHUB_TOKEN'))
-    event = read_json(os.getenv('GITHUB_EVENT_PATH'))
-    # branch_label = event['pull_request']['head']['label']  # author:branch
-    # branch_name = branch_label.split(':')[-1]
-    repo = gh.get_repo(event['repository']['full_name'])
-    prs = repo.get_pulls(state='open', sort='created')
-    pr = prs[0]
-    # filenamevalidation = invalid_file_names_Stirng
-    # # load template
-    # template = load_template(get_actions_input('filename'))
-
-    # # build a comment
-    # pr_info = {
-    #     'filenamevalidation':filenamevalidation,
-    #     'pull_id': pr.number,
-    #     'branch_name': branch_name
-    # }
-    # new_comment = template.format(**pr_info)
-
-    # # check if this pull request has a duplicated comment
-    # old_comments = [c.body for c in pr.get_issue_comments()]
-    # if new_comment in old_comments:
-    #     print('This pull request already a duplicated comment.')
-    #     exit(0)
-
-    # add the comment
-    pr.create_issue_comment('invalid FILE NAME: ',invalid_file_names_Stirng)
+    print (type(invalid_file_names_Stirng))
 
 
 
