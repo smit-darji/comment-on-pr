0bb74976bad655a8a18d69c35720ac2abbda7788
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-20 13:51:45 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -4,7 +4,7 @@ import os
 
 from github import Github
 # from main import invalid_file_names
-
+filename = "template.md"
 
 def read_json(filepath):
     """
@@ -62,6 +62,7 @@ def load_template(filename):
     template : str
 
     """
+
     print("file is:",filename)
     print(type(filename))
     template_path = os.path.join('.github/workflows', filename)
