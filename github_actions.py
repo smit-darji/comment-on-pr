9c7a366d5086d0a4c8b92a3c317d0e45daa53a8f
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-09-21 10:50:25 +0530

    first commit

--- github_actions.py
+++ github_actions.py
@@ -76,7 +76,7 @@ def post_pr_comment (github_client, invalid_file_names, invalid_directory_names)
     print("invalid_file_names" ,invalid_file_names)
     print("invalid_directory_names" ,invalid_directory_names)
     invalid_file_names_Stirng = "\n  \t \t    ".join( invalid_file_names )
-    invalid_directory_names_Stirng = "\n \t \t    ".join( invalid_directory_names )
+    invalid_directory_names_Stirng = "\n \t \t \t".join( invalid_directory_names )
         # search a pull request that triggered this action
     gh = Github(os.getenv('GITHUB_TOKEN'))
     event = read_json(os.getenv('GITHUB_EVENT_PATH'))
